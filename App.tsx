c69f7af05a1ebee666ba50be246c44c19fd8e7c9
Bala Subrahmanyam Varanasi <vabasu@gmail.com>
2022-10-25 00:42:14 +0530

    feat: add basic react-native-router and react-native-screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { NativeBaseProvider, Text, Box } from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <NativeBaseProvider>
+        <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+          <Text>Hellow World!</Text>
+        </Box>
+      </NativeBaseProvider>
+    </NavigationContainer>
   );
 }
 
